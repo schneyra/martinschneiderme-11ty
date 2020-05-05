3381c1aff8232385f744e032cb05dc53cab89add
Martin Schneider <martin@martinschneider.me>
2020-05-05 16:03:14 +0200

    Sharing-Button

--- website/_source/js/articleSharing.js
+++ website/_source/js/articleSharing.js
@@ -1,11 +1,11 @@
 // https://web.dev/web-share/
 (() => {
-    const sharingButton = document.querySelector("[data-sharing-button]");
+    if (navigator.share) {
+        const sharingButton = document.querySelector("[data-sharing-button]");
 
-    sharingButton.innerHTML = "share (can share: " + navigator.share + ")";
+        sharingButton.classList.remove("is-hidden");
 
-    sharingButton.addEventListener("click", () => {
-        if (navigator.share) {
+        sharingButton.addEventListener("click", () => {
             let url = document.location.href;
 
             const canonicalElement = document.querySelector(
@@ -20,6 +20,6 @@
                 .share({ url: url })
                 .then(() => console.log("Successful share"))
                 .catch((error) => console.log("Error sharing", error));
-        }
-    });
+        });
+    }
 })();
