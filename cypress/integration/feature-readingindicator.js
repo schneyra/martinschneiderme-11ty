1d544f9bcdaa1132709af1947c96b51db18d96a5
Martin Schneider <martin@martinschneider.me>
2020-05-22 22:05:05 +0200

    Cleanup

---
+++ cypress/integration/feature-readingindicator.js
@@ -0,0 +1,14 @@
+describe("Feature - Reading-Indicator", () => {
+    beforeEach(() => {
+        cy.visit("/articles/welcome-to-my-new-website/");
+    });
+
+    it("starts with 0%", () => {
+        cy.contains("[data-cy-reading-indicator]", "0%");
+    });
+
+    it("ends with value 100%", () => {
+        cy.scrollTo("bottom");
+        cy.contains("[data-cy-reading-indicator]", "100%");
+    });
+});
