12d7c2cce48fa89908e6f4fde554ab1cb3cadfad
Martin Schneider <martin@martinschneider.me>
2020-05-23 21:02:50 +0200

    Fix test?

--- cypress/integration/feature-colormode.js
+++ cypress/integration/feature-colormode.js
@@ -1,14 +1,12 @@
 describe("Feature - Colormode-Toggle", () => {
-    beforeEach(() => {
-        cy.visit("/");
-    });
-
     it("switches to dark mode", () => {
+        cy.visit("/");
         cy.get("[data-cy=colorModeToggleButton]").click();
         cy.get("[data-user-color-scheme]").its("value").should("be", "dark");
     });
 
-    it("switches to light mode", () => {
+    it("switches to dark and back to light mode", () => {
+        cy.visit("/");
         cy.get("[data-cy=colorModeToggleButton]").click();
         cy.get("html").should("have.attr", "data-user-color-scheme", "dark");
         cy.get("[data-cy=colorModeToggleButton]").click();
