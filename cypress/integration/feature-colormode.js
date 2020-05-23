6e4914fe2c83bc849535f37e74d8b211f3335d0d
Martin Schneider <martin@martinschneider.me>
2020-05-23 21:41:41 +0200

    Consolidate tests

--- cypress/integration/feature-colormode.js
+++ cypress/integration/feature-colormode.js
@@ -1,7 +1,9 @@
 describe("Feature - Colormode-Toggle", () => {
-    it("switches to dark mode", () => {
+    it("switches to dark and back to light mode", () => {
         cy.visit("/");
         cy.get("[data-cy=colorModeToggleButton]").click();
-        cy.get("[data-user-color-scheme]").its("value").should("be", "dark");
+        cy.get("html").should("have.attr", "data-user-color-scheme", "dark");
+        cy.get("[data-cy=colorModeToggleButton]").click();
+        cy.get("html").should("not.have.attr", "data-user-color-scheme");
     });
 });
