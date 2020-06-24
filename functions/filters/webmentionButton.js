ce37263fcc8015291a1321813eab6bc80b6958ff
Martin Schneider <martin@martinschneider.me>
2020-06-24 21:28:20 +0200

    Bugfixing Webmentions

--- functions/filters/webmentionButton.js
+++ functions/filters/webmentionButton.js
@@ -11,11 +11,13 @@ async function getWebmentionCount(slug) {
 }
 
 function generateWebmentionHtml(count, slug) {
+    const label = count.count === 1 ? "Reaction" : "Reactions";
+
     let html = `
     <button class="icon-button icon-button--has-text" data-webmentionbutton="${slug}">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon-button__icon" aria-hidden="true" focusable="false" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg>
         <span class="screen-reader-text">Show</span>
-        <span class="icon-button__text">${count.count} Reactions</span>
+        <span class="icon-button__text">${count.count} ${label}</span>
     </button>`;
 
     return html;
