780a0798cfd455b6c63a36aa0484d85ae119da67
Martin Schneider <martin@martinschneider.me>
2020-08-01 21:46:27 +0200

    fix tests

--- cypress/integration/page-homepage.js
+++ cypress/integration/page-homepage.js
@@ -7,15 +7,15 @@ describe("Page - Homepage", () => {
         cy.contains("h1", "Hey, my name is Martin!");
     });
 
-    it("navigates to /about", () => {
-        cy.get("nav a").contains("about").click();
-        cy.url().should("include", "/about");
-        cy.contains("h1", "Hello, again!");
-    });
-
     it("navigates to /articles", () => {
         cy.get("nav a").contains("articles").click();
         cy.url().should("include", "/articles");
         cy.contains("h1", "Articles");
+    });
+
+    it("navigates to /blogroll", () => {
+        cy.get("nav a").contains("blogroll").click();
+        cy.url().should("include", "/blogroll");
+        cy.contains("h1", "Blogroll");
     });
 });
