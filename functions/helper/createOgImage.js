4e604d98f27c9f6f843b113b77886b08aaea7a95
Martin Schneider <mail@schneyra.de>
2021-02-26 21:18:47 +0000

    make ogimage bigger

--- functions/helper/createOgImage.js
+++ functions/helper/createOgImage.js
@@ -12,24 +12,24 @@ module.exports = async function createOgImage(title) {
     }
 
     let svg = (firstPartOfTitle, titleAsArray) => `
-            <svg width="1200" height="600" viewBox="0 0 1200 600" xmlns="http://www.w3.org/2000/svg">
+        <svg width="2400" height="1200" viewBox="0 0 1200 600" xmlns="http://www.w3.org/2000/svg">
             <rect width="1200" height="600" fill="#0f131a"/> 
-            <rect width="50" height="50" x="1050" y="25" fill="#87d4d4"/> 
-            <rect width="50" height="50" x="1125" y="25" fill="#ff540a"/> 
-            <text x="75" y="410" text-anchor="left" font-size="40px" font-family="sans-serif" fill="#f3f3f3" text-rendering="optimizeLegibility">${firstPartOfTitle.join(
+            <rect width="100" height="100" x="1025" y="50" fill="#87d4d4"/> 
+            <rect width="100" height="100" x="1050" y="75" fill="#ff540a"/>
+            <text x="75" y="390" text-anchor="left" font-size="50px" font-family="sans-serif" fill="#f3f3f3" text-rendering="optimizeLegibility">${firstPartOfTitle.join(
                 " "
             )}</text>
-            <text x="75" y="465" text-anchor="left" font-size="40px" font-family="sans-serif" fill="#f3f3f3" text-rendering="optimizeLegibility">${titleAsArray.join(
+            <text x="75" y="445" text-anchor="left" font-size="50px" font-family="sans-serif" fill="#f3f3f3" text-rendering="optimizeLegibility">${titleAsArray.join(
                 " "
             )}</text>
-            <text x="75" y="530" text-anchor="left" font-size="20px" font-family="monospace" fill="#87d4d4" text-rendering="optimizeLegibility">martinschneider.me</text>
-            </svg>
-            `;
+            <text x="75" y="520" text-anchor="left" font-size="30px" font-family="monospace" fill="#87d4d4" text-rendering="optimizeLegibility">martinschneider.me</text>
+        </svg>
+        `;
 
     var buffer = Buffer.from(svg(firstPartOfTitle, titleAsArray));
 
     let options = {
-        widths: [1200],
+        widths: [2400],
         formats: ["jpg"],
         outputDir: "./_site/images/og",
         urlPath: "/images/og/"
