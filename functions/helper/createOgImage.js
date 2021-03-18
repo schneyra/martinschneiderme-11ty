273cd446f9b3af00e385a1e72d5358944009ad6e
Martin Schneider <mail@schneyra.de>
2021-03-18 21:00:40 +0000

    switches colors to hsl, makes my face bigger

--- functions/helper/createOgImage.js
+++ functions/helper/createOgImage.js
@@ -13,16 +13,16 @@ module.exports = async function createOgImage(title) {
 
     let svg = (firstPartOfTitle, titleAsArray) => `
         <svg width="2400" height="1200" viewBox="0 0 1200 600" xmlns="http://www.w3.org/2000/svg">
-            <rect width="1200" height="600" fill="#0f131a"/> 
-            <rect width="100" height="100" x="1025" y="50" fill="#87d4d4"/> 
-            <rect width="100" height="100" x="1050" y="75" fill="#ff540a"/>
-            <text x="75" y="390" text-anchor="left" font-size="50px" font-family="sans-serif" fill="#f3f3f3" text-rendering="optimizeLegibility">${firstPartOfTitle.join(
+            <rect width="1200" height="600" fill="hsl(218, 27%, 8%)"/> 
+            <rect width="100" height="100" x="1025" y="50" fill="hsl(180, 50%, 76%)"/> 
+            <rect width="100" height="100" x="1050" y="75" fill="hsl(18, 100%, 52%)"/>
+            <text x="75" y="390" text-anchor="left" font-size="50px" font-family="sans-serif" fill="hsl(0, 0%, 95%)" text-rendering="optimizeLegibility">${firstPartOfTitle.join(
                 " "
             )}</text>
-            <text x="75" y="445" text-anchor="left" font-size="50px" font-family="sans-serif" fill="#f3f3f3" text-rendering="optimizeLegibility">${titleAsArray.join(
+            <text x="75" y="445" text-anchor="left" font-size="50px" font-family="sans-serif" fill="hsl(0, 0%, 95%)" text-rendering="optimizeLegibility">${titleAsArray.join(
                 " "
             )}</text>
-            <text x="75" y="520" text-anchor="left" font-size="30px" font-family="monospace" fill="#87d4d4" text-rendering="optimizeLegibility">martinschneider.me</text>
+            <text x="75" y="520" text-anchor="left" font-size="30px" font-family="monospace" fill="hsl(180, 47%, 68%)" text-rendering="optimizeLegibility">martinschneider.me</text>
         </svg>
         `;
 
