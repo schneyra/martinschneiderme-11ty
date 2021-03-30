266c1a61068063e51167f24a3921579eadbf4a9d
Martin Schneider <martin@martinschneider.me>
2021-03-30 23:33:26 +0200

    prevent htmlmin from transforming doctype

--- functions/transforms/htmlmin.js
+++ functions/transforms/htmlmin.js
@@ -3,7 +3,6 @@ const htmlmin = require("html-minifier");
 module.exports = (content, outputPath) => {
     if (outputPath.endsWith(".html")) {
         let minified = htmlmin.minify(content, {
-            useShortDoctype: true,
             removeComments: true,
             collapseWhitespace: true,
             removeScriptTypeAttributes: true,
