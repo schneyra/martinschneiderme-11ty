c8391e96e7092d8c1a7c87c03878a638dedc2df8
Martin Schneider <martin@martinschneider.me>
2021-04-22 01:34:18 +0200

    article images with 11ty images

--- website/_data/bookmarks.js
+++ website/_data/bookmarks.js
@@ -82,6 +82,11 @@ async function generateTagList(tags, bookmarks) {
 }
 
 module.exports = async () => {
+    return {
+        bookmarks: [],
+        tags: []
+    };
+
     const bookmarksFromPinboard = await fetchBookmarks();
     const tagsFromPinboard = await fetchTags();
     const processedBookmarks = await processBookmarks(bookmarksFromPinboard);
