5c39267bb8f2d9137edfba1ad2ef98ebea5119ab
Martin Schneider <mail@schneyra.de>
2022-01-09 13:25:30 +0000

    switching SCSS to custom 11ty-extension

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const sass = require("sass");
 
 const w3DateFilter = require("./website/_functions/filters/w3cDate.js");
 const longDate = require("./website/_functions/filters/longDate.js");
@@ -43,6 +44,31 @@ module.exports = function (eleventyConfig) {
         "imageForArticles",
         imageShortcodeForArticles
     );
+
+    // SCSS- and JS-Compilation thanks to David Darnes from:
+    // https://gist.github.com/daviddarnes/8d70d7b8eaee474bcb19e30fc45e63ff
+    eleventyConfig.addTemplateFormats("scss");
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css",
+        compile: function (contents, inputPath) {
+            if (inputPath.startsWith(`./website/_`)) {
+                return;
+            }
+
+            let includesPaths = this.config.dir.includes;
+
+            return (data) => {
+                let ret = sass.renderSync({
+                    file: inputPath,
+                    includesPaths: ["website/_source", includesPaths],
+                    data: contents
+                });
+
+                console.log(`[msme] SCSS compiled (${inputPath})`);
+                return ret.css.toString("utf8");
+            };
+        }
+    });
 
     // Transforms run after HTML-generation
     if (process.env.ELEVENTY_ENV === "production") {
