84ceee69dc53d4829d6b29628b2071bb1ac484ca
Martin Schneider <mail@schneyra.de>
2023-06-27 20:03:59 +0000

    removes a lot of clutter

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,11 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
-const { EleventyEdgePlugin } = require("@11ty/eleventy");
-const path = require("path");
-const terser = require("terser");
 
 const w3DateFilter = require("./website/_functions/filters/w3cDate.js");
 const longDate = require("./website/_functions/filters/longDate.js");
 const recentArticles = require("./website/_functions/filters/recentArticles.js");
 const htmlmin = require("./website/_functions/transforms/htmlmin");
-const purgeInlineCSS = require("./website/_functions/transforms/purgeInlineCSS");
 const pictureElementShortcode = require("./website/_functions/filters/pictureElementShortcode");
 const figureShortcodeForArticles = require("./website/_functions/filters/figureShortcodeForArticles");
 const createOgImage = require("./website/_functions/filters/createOgImage");
@@ -20,20 +16,8 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPlugin(directoryOutputPlugin);
-    eleventyConfig.addPlugin(EleventyEdgePlugin);
 
     eleventyConfig.setQuietMode(true);
-
-    // FILE HANDLING
-    eleventyConfig.setTemplateFormats(["ico", "njk", "opml", "md", "css"]);
-    eleventyConfig.addWatchTarget("./website/articles");
-
-    eleventyConfig.addWatchTarget("./website/_source");
-    eleventyConfig.addPassthroughCopy("./website/fonts");
-    eleventyConfig.addPassthroughCopy("./website/images");
-    eleventyConfig.addPassthroughCopy("./website/main.css");
-    eleventyConfig.addPassthroughCopy("./website/prism.css");
-    eleventyConfig.addWatchTarget("./website/*.css");
 
     eleventyConfig.addPassthroughCopy({
         "./node_modules/instant.page/instantpage.js": "instantpage.js",
@@ -47,13 +31,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addFilter("recentArticles", recentArticles);
     eleventyConfig.addFilter("stripTags", stripTags);
     eleventyConfig.addNunjucksAsyncFilter("createOgImage", createOgImage);
-    eleventyConfig.addNunjucksAsyncFilter(
-        "mangleJs",
-        async (code, callback) => {
-            const mangledCode = await terser.minify(code);
-            callback(null, mangledCode.code);
-        }
-    );
 
     eleventyConfig.addNunjucksAsyncShortcode(
         "pictureElement",
@@ -65,26 +42,8 @@ module.exports = function (eleventyConfig) {
         figureShortcodeForArticles
     );
 
-    eleventyConfig.addTemplateFormats("js");
-    eleventyConfig.addExtension("js", {
-        outputFileExtension: "min.js",
-        compile: function (contents, inputPath) {
-            let parsed = path.parse(inputPath);
-
-            if (parsed.name.startsWith("_") || parsed.dir !== "./website/js") {
-                return;
-            }
-
-            return async () => {
-                let ret = await terser.minify(contents);
-                console.log(`[msme] JS compiled (${inputPath})`);
-                return ret.code;
-            };
-        }
-    });
-
+    
     // TRANSFORMS
-    //eleventyConfig.addTransform("purgeInlineCSS", purgeInlineCSS);
     eleventyConfig.addTransform("htmlmin", htmlmin);
 
     return {
