00b46dc89dffdd9667b3356b988d8ce9242f280a
Martin Schneider <mail@schneyra.de>
2023-07-14 08:57:43 +0000

    uses EleventyFetch instead of fetch

--- website/_data/webmentions.js
+++ website/_data/webmentions.js
@@ -1,12 +1,18 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
 const helper = require("./helper");
 
 async function webmentionsForUrl() {
-    let url = `https://webmention.io/api/mentions.jf2?domain=martinschneider.me&per-page=100000&token=${helper.webmention_io_token}`;
+    try {
+        let url = `https://webmention.io/api/mentions.jf2?domain=martinschneider.me&per-page=100000&token=${helper.webmention_io_token}`;
 
-    const response = await fetch(url);
-    const mentions = await response.json();
-
-    return mentions;
+        return EleventyFetch(url, {
+            duration: "10m",
+            type: "json"
+        });
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        return [];
+    }
 }
 
 module.exports = async () => {
